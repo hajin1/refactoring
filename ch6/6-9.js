a02124b6efceb18d4d052699d59f1c101d82045b
hajin <hajinkim@kangwon.ac.kr>
2022-12-04 23:00:45 +0900

    6-9 여러 함수를 클래스로 묶기

--- ch6/6-9.js
+++ ch6/6-9.js
@@ -1,10 +1,43 @@
-const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
-
-export function acquireReading() {
-  return reading;
+export class Reading {
+	#customer;
+	#quantity;
+	#month;
+	#year;
+	constructor({ customer, quantity, month, year }) {
+		this.#customer = customer;
+		this.#quantity = quantity;
+		this.#month = month;
+		this.#year = year;
+	}
+	get customer() {
+		return this.#customer;
+	}
+	get quantity() {
+		return this.#quantity;
+	}
+	get month() {
+		return this.#month;
+	}
+	get year() {
+		return this.#year;
+	}
+	get baseRate() {
+		if (this.year === 2017 && this.month === 5) return 0.1;
+		return 0.2;
+	}
+	get baseCharge() {
+		return this.baseRate * this.quantity;
+	}
+	get taxThreshold() {
+		return 0.1;
+	}
+	get taxableCharge() {
+		Math.max(0, this.baseCharge - this.taxThreshold);
+	}
 }
 
-export function baseRate(month, year) {
-  if (year === 2017 && month === 5) return 0.1;
-  return 0.2;
+const reading = new Reading({ customer: 'ivan', quantity: 10, month: 5, year: 2017 });
+
+export function acquireReading() {
+	return reading;
 }
