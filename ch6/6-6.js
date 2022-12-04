50ffa0878e5fb25e90411e693b6839f0a46073db
hajin <hajinkim@kangwon.ac.kr>
2022-12-04 22:45:32 +0900

    6-6 변수 캡슐화 하기

--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,5 +1,21 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+class Person {
+	#lastname;
+	#firstname;
+
+	constructor(firstName, lastName) {
+		this.#firstname = firstName;
+		this.#lastname = this.#lastname;
+	}
+	get firstName() {
+		return this.#firstname;
+	}
+	get lastName() {
+		return this.#lastname;
+	}
+}
+
+let defaultOwner = new Person('마틴', '파울러');
 
 export function getDefaultOwner() {
-  return defaultOwner;
+	return defaultOwner;
 }
